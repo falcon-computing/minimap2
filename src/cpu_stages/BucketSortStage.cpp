5b743e01e28905fe9cb57136195544aff20942d5
TianJin297 <jt492177512@gmail.com>
2019-03-07 14:49:36 -0800

    fix interval calculation error

--- src/cpu_stages/BucketSortStage.cpp
+++ src/cpu_stages/BucketSortStage.cpp
@@ -18,6 +18,63 @@
 #define MAP_IN_PAIR_FLAG 2
 #define NOT_PRIM_FLAG 256
 
+std::vector<std::vector<int64_t>> BucketSortStage::get_intervals(int64_t start, int64_t end) {
+  if (start < 0 || end < 0) throw("pos less than 0");
+  if (start > end) throw("start pos larger than end");
+  if (start > accumulate_length_[head_->n_targets]) throw("start pos out of reference boundary");
+  std::vector<std::vector<int64_t>> res;
+  std::vector<int64_t> tmp;
+  // calculate the start contig id;
+  int contig_id = 0;
+  while (contig_id < head_->n_targets && 
+        start >= accumulate_length_[contig_id + 1]) {
+    contig_id ++;
+  }
+  while (contig_id < head_->n_targets &&
+        end > accumulate_length_[contig_id + 1]) {
+    tmp.push_back(contig_id);
+    tmp.push_back(start - accumulate_length_[contig_id]);
+    tmp.push_back(accumulate_length_[contig_id + 1] - accumulate_length_[contig_id]);
+    res.push_back(tmp);
+    tmp.clear();
+    start = accumulate_length_[contig_id + 1];
+    contig_id ++;
+  }
+  if (contig_id < head_->n_targets) {
+    tmp.push_back(contig_id);
+    tmp.push_back(start - accumulate_length_[contig_id]);
+    tmp.push_back(end - accumulate_length_[contig_id]);
+    res.push_back(tmp);
+    tmp.clear();
+  }
+  return res;
+}
+
+int BucketSortStage::bucket_id_calculate(int32_t contig_id, int32_t read_pos) {
+  int64_t acc_pos = accumulate_length_[contig_id] + read_pos;
+  int large_bucket = (accumulate_length_[head_->n_targets]%num_buckets_)?
+                      (accumulate_length_[head_->n_targets]%num_buckets_):
+                      num_buckets_;
+  int64_t limit = large_bucket * bucket_size_;
+  return (acc_pos > limit)?
+        (
+          (bucket_size_ - 1)?
+          (large_bucket + (acc_pos - limit)/(bucket_size_ - 1)):
+          (large_bucket)
+        ):
+        (acc_pos/bucket_size_);
+}
+
+int BucketSortStage::get_bucket_id(bam1_t* read) {
+  if (read->core.tid == -1) {
+
+    return num_buckets_;
+  }
+  int32_t contig_id = read->core.tid;
+  int32_t read_pos = read->core.pos;
+  return bucket_id_calculate(contig_id, read_pos);
+}
+
 BucketSortStage::BucketSortStage(
     bam_hdr_t* head, 
     std::string out_dir, 
@@ -40,20 +97,16 @@ BucketSortStage::BucketSortStage(
   accumulate_length_.resize(head->n_targets+1);
   accumulate_length_[0] = 0;
   for (int i = 0; i < head_->n_targets; i++) {
-    accumulate_length_[i+1] = head_->target_len[i] + 
-      accumulate_length_[i];
+    accumulate_length_[i+1] = head_->target_len[i] + accumulate_length_[i];
   }
   int64_t total_length = accumulate_length_[head_->n_targets];
   bucket_size_ = (total_length + num_buckets - 1) / num_buckets;
-
-  int contig_start_pos = 0;
-  int contig_id = 0;
+  int large_bucket = total_length%num_buckets;
+  int64_t contig_start_pos = 0;
 
   const char *modes[] = {"wb", "wb0", "w"};
   // the last bucket is for unmapped reads
   for (int i = 0; i <= num_buckets_; i++) {
-    //boost::any var = this->getConst("sam_dir");
-    //std::string out_dir = boost::any_cast<std::string>(var);
     std::stringstream ss; 
     ss << out_dir << "/part-" << std::setw(6) << std::setfill('0') << i;
     std::string bucket_fname = ss.str() + ".bam";
@@ -66,22 +119,12 @@ BucketSortStage::BucketSortStage(
 
     // create interval files
     std::ofstream intv_file(intv_fname.c_str());
-
-    int end = contig_start_pos + bucket_size_;
-    while (end > head_->target_len[contig_id]) {
-      intv_file << head_->target_name[contig_id] << "\t" 
-                << contig_start_pos << "\t" 
-                << head_->target_len[contig_id] << "\n";
-      end -= head_->target_len[contig_id];
-      contig_start_pos = 0;
-      contig_id ++;
-
-      if (contig_id == head_->n_targets) break;
-    }
-    if (contig_id < head_->n_targets) {
-      intv_file << head_->target_name[contig_id] << "\t" 
-                << contig_start_pos << "\t" 
-                << end << "\n";
+    int64_t end = contig_start_pos + bucket_size_ - (int)(i >= large_bucket);
+    std::vector<std::vector<int64_t>> intv_vec_vec = get_intervals(contig_start_pos, end);
+    for (auto & intv_vec : intv_vec_vec) {
+      intv_file << head_->target_name[intv_vec[0]] << "\t"
+                << intv_vec[1] << "\t"
+                << intv_vec[2] << "\n";
     }
     contig_start_pos = end;
     intv_file.close();
@@ -92,16 +135,6 @@ void BucketSortStage::closeFiles() {
   for (auto it = buckets_.begin(); it != buckets_.end(); ++it) {
     delete it->second;
   }
-}
-
-int BucketSortStage::get_bucket_id(bam1_t* read) {
-  int32_t contig_id = read->core.tid;
-  int32_t read_pos = read->core.pos;
-  if (read->core.tid == -1) {
-    return num_buckets_;
-  }
-  int64_t acc_pos = accumulate_length_[contig_id] + read_pos;
-  return (acc_pos-1)/bucket_size_;
 }
 
 int BucketSortStage::compute(BamsBatch const & input) {
