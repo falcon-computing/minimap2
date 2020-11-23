767595c65a0d528ba2f0b24658a101cd539942df
Gary Bai <guangzebai@falcon-computing.com>
2020-11-23 08:52:52 -0800

    Check in 3rd-party packages

--- cmake/FindGoogle.cmake
+++ cmake/FindGoogle.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(glog-download
     PREFIX "glog"
-    URL https://s3.amazonaws.com/fcs-build-public/glog-falcon.tar.gz
+    URL ${CMAKE_SOURCE_DIR}/deps/glog-falcon.tar.gz
     URL_MD5 2b1bb4285ef4c8963d5e0e338f1952b8
     SOURCE_DIR "${CMAKE_BINARY_DIR}/glog/install"
     CONFIGURE_COMMAND ""
@@ -9,7 +9,7 @@ ExternalProject_Add(glog-download
 
 ExternalProject_Add(gflags-download
     PREFIX "gflags"
-    URL https://s3.amazonaws.com/fcs-build-public/gflags.tar.gz
+    URL ${CMAKE_SOURCE_DIR}/deps/gflags.tar.gz
     URL_MD5 1de8187489fbced5cc86c2ba241440e4
     SOURCE_DIR "${CMAKE_BINARY_DIR}/gflags/install"
     CONFIGURE_COMMAND ""
@@ -18,7 +18,7 @@ ExternalProject_Add(gflags-download
 
 ExternalProject_Add(googletest-download
     PREFIX "googletest"
-    URL https://s3.amazonaws.com/fcs-build-public/googletest.tar.gz
+    URL ${CMAKE_SOURCE_DIR}/deps/googletest.tar.gz
     URL_MD5 18fda945045354e264e3cca5428525d6
     SOURCE_DIR "${CMAKE_BINARY_DIR}/googletest/install"
     CONFIGURE_COMMAND ""
@@ -27,8 +27,7 @@ ExternalProject_Add(googletest-download
 
 ExternalProject_Add(protobuf-download
     PREFIX "protobuf"
-    URL https://s3.amazonaws.com/fcs-build-public/protobuf-2.5.0.tar.gz
-    URL_MD5 a68c7ee81a65a84e57287af2a0738a75
+    URL ${CMAKE_SOURCE_DIR}/deps/protobuf-2.5.0.tar.gz
     SOURCE_DIR "${CMAKE_BINARY_DIR}/protobuf/install"
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
